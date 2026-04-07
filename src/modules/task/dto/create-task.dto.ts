b3a5615492a9ffbd34f064f9998546df019b8dc9
Cristian-Oropeza <oropezacristian40@gmail.com>
2026-04-07 01:17:18 -0600

    Fix: Uso de custom errors y creación de logs

--- src/modules/task/dto/create-task.dto.ts
+++ src/modules/task/dto/create-task.dto.ts
@@ -17,7 +17,7 @@ export class CreateTaskDto {
   @MinLength(3)
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @MaxLength(100)
-  name: string;
+  name!: string;
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsString()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -26,15 +26,15 @@ export class CreateTaskDto {
   @MinLength(3)
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @MaxLength(250)
-  description: string;
+  description!: string;
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNotEmpty()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsBoolean()
-  priority: boolean;
+  priority!: boolean;
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNumber()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsInt()
-  user_id: number;
+  user_id!: number;
 }
